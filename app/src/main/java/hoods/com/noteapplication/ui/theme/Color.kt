0e8965a511bbcd03b069eeb74e0d5a9d4309a853
hood <hoods.tech@gmail.com>
2023-08-05 15:35:37 +0300

    Initial Project

---
+++ app/src/main/java/hoods/com/noteapplication/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package hoods.com.noteapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val DarkPrimary = Color(0xFF36A2EB) // Vibrant blue
+val DarkSecondary = Color(0xFF5DD38A) // Fresh green
+val DarkTertiary = Color(0xFFEFB8C8) // Pink
+val DarkBackground = Color(0xFF1A1A2C) // Purple Dark
+val DarkSurface = Color(0xFF2D2D4D) // Dark gray
+val DarkOnSurface = Color(0xFFEAEAEA) // Light gray (Text and icons on the surface)
+val DarkSurfaceVariant = Color(0xFF26264D) // Slightly lighter purple
+
+val LightPrimary = Color(0xFF36A2EB) // Vibrant blue
+val LightSecondary = Color(0xFF5DD38A) // Fresh green
+val LightTertiary = Color(0xFFEFB8C8) // Pink
+val LightBackground = Color(0xFFF5F5F5) // Off-white
+val LightSurface = Color(0xFFE0E0E0) // Dark gray
+val LightOnSurface = Color(0xFF444444) // Dark gray (Text and icons on the surface)
+val LightSurfaceVariant = Color(0xFFD1D1E0) // Slightly lighter purple
